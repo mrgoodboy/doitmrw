9a78fd9a0eac6c74764109f8725000c785a832f8
Graham Kaemmer <graham.kaemmer@yale.edu>
2013-11-09 10:43:02 -0500

    fix fuckups

--- app/assets/javascripts/owl-messages.js
+++ app/assets/javascripts/owl-messages.js
@@ -1,40 +1,36 @@
 var messages = [
-	"Should I do my Math homework?",
-	"Should I do my English paper?",
-	"Should I go work out?",
-	"Should I clean my room?",
-	"Should I brush my teeth?",
-	"Should I take a shit?",
-	"Should I take a piss?",
-	"Should I go to class?",
-	"Should I go buy food?",
-	"Should I go for a run?",
+	"do my Math homework",
+	"do my English paper",
+	"go work out",
+	"clean my room",
+	"brush my teeth",
+	"take a shit",
+	"take a piss",
+	"go to class",
+	"go buy food",
+	"go for a run"
 	];
 
 
 
 function generateMessage (message) {
-	
-	$(".owl-message-box").html("<p class='owl-message'>"+message+"</p>");
-	$(".owl-message-box").delay(1000).fadeIn(1000).delay(1000).fadeOut(1000, function() {
-		$(".owl-message-box").html("<p class='owl-message'> Nah, I'll do it tmrw. </p>")
-		$(".owl-message-box").fadeIn(1000).delay(1000).fadeOut(1000, function() { 
-			$('.owl-message-box').text(''); 
-		});
+	ticker = $('#ticker-field');
+	ticker.val(message);
+	ticker.css({color: 'rgba(0, 0, 0, 0)'});
+	ticker.animate({color: 'rgba(0, 0, 0, 1)'}, 1000).delay(2000).animate({color: 'rgba(0, 0, 0, 0)'}, 1000, function() {
+		message2 = message;
+		while (message2 == message) {
+			message2 = messages[Math.floor((Math.random()*messages.length))];
+		}
+		generateMessage(message2);
 	});
-	
-};
+}
 
 
 
 $(document).ready(function() {
 
-	if ($(".owl-message-box").children().length === 0) {
-		generateMessage(messages[Math.floor((Math.random()*messages.length))]);
-	} else {
-		$(".owl-message-box").fadeIn(1000).delay(1000).fadeOut(1000);
-	}
-
+	generateMessage(messages[Math.floor((Math.random()*messages.length))]);
 
 });
 
