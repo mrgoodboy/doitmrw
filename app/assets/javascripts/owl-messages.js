3c6c4e6f6a92b5beb49d5301ee5ddadf1d04a4ad
Graham Kaemmer <graham.kaemmer@yale.edu>
2013-11-09 12:44:15 -0500

    font change to front page

--- app/assets/javascripts/owl-messages.js
+++ app/assets/javascripts/owl-messages.js
@@ -1,36 +1,33 @@
 var messages = [
-"do my Math homework",
-"do my English paper",
-"go work out",
-"clean my room",
-"brush my teeth",
-
-"go to class",
-"go buy food",
-"go for a run",
-"what does the owl say",
-"write my English paper",
-"do work",
-"get off this site",
-"take a shower",
-"go buy groceries",
-"stop playing video games",
-"check my email",
-"go on a run",
-"go to the gym",
-"go to class",
-"do my homework",
-"get a haircut",
-"brush my teeth",
-"leave my room",
-"do my problem set",
-"code an app",
-"sleep",
-"eat",
-"play a sport",
-"watch TV",
-"talk to people",
-"be productive"
+"do my Math homework?",
+"clean my room?",
+"brush my teeth?",
+"go to class?",
+"go buy food?",
+"go for a run?",
+"what does the owl say?",
+"write my English paper?",
+"do work?",
+"get off this site?",
+"take a shower?",
+"go buy groceries?",
+"stop playing video games?",
+"check my email?",
+"go on a run?",
+"go to the gym?",
+"go to class?",
+"do my homework?",
+"get a haircut?",
+"brush my teeth?",
+"leave my room?",
+"do my problem set?",
+"code an app?",
+"sleep?",
+"eat?",
+"play a sport?",
+"watch TV?",
+"talk to people?",
+"be productive?"
 ];
 
 
@@ -38,8 +35,8 @@ var messages = [
 function generateMessage (message) {
 	ticker = $('#ticker-field');
 	ticker.text(message);
-	ticker.css({color: 'rgba(0, 0, 0, 0)'});
-	ticker.animate({color: 'rgba(0, 0, 0, 1)'}, 1000).delay(2000).animate({color: 'rgba(0, 0, 0, 0)'}, 1000, function() {
+	ticker.css({color: 'rgba(70, 70, 70, 0)'});
+	ticker.animate({color: 'rgba(70, 70, 70, 1)'}, 1000).delay(2000).animate({color: 'rgba(0, 0, 0, 0)'}, 1000, function() {
 		message2 = message;
 		while (message2 == message) {
 			message2 = messages[Math.floor((Math.random()*messages.length))];
