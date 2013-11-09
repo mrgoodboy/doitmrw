727da928de7a1665e0f5434326dac8bb59b0d94e
Graham Kaemmer <graham.kaemmer@yale.edu>
2013-11-09 18:55:03 -0500

    the great edge fix of 2013 2

--- app/models/edge.rb
+++ app/models/edge.rb
@@ -1,5 +1,5 @@
 class Edge < ActiveRecord::Base
-  attr_accessible :weight, :user, :content, :user_id, :content_id
+  attr_accessible :weight, :user, :content, :user_id, :content_id, :category_id
 
   belongs_to :user
   belongs_to :content
