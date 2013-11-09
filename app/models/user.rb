49f42c86a7fc29e70418dd2754591f6b1c70dfd9
Graham Kaemmer <graham.kaemmer@yale.edu>
2013-11-09 15:58:49 -0500

    fix stupid association mistake

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ActiveRecord::Base
   attr_accessible :name, :provider, :uid, :email, :guest
 
   has_many :edges
-  has_many :content, through: :edges
+  has_many :content
 
   devise :omniauthable, :omniauth_providers => [:facebook]
 
